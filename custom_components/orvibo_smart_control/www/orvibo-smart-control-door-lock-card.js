297aba3b5557e1b0c4501794b226d646d37e3b5a
maycode0-0 <maycode0-0@users.noreply.github.com>
2026-08-11 16:25:39 +0800

    chore: release v0.1.2

--- custom_components/orvibo_smart_control/www/orvibo-smart-control-door-lock-card.js
+++ custom_components/orvibo_smart_control/www/orvibo-smart-control-door-lock-card.js
@@ -12,7 +12,7 @@
  */
 
 const ORVIBO_PREFIX = "orvibo_smart_control_";
-const CARD_VERSION = "0.1.1";
+const CARD_VERSION = "0.1.2";
 
 class OrviboSmartControlDoorLockCard extends HTMLElement {
   constructor() {
